d51f21945387f9e34806f216dd2e1271ed5e3e3f
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2016-12-19 15:28:59 +0100

    add R scripts for creating MNW2 csv files based on GIS data (create_mnw2_csvs.R) & animation of annual wellfield abstraction (Animation.R), requires that required .dbf files are saved in subfolder "/GIS"

--- GIS/Animation.R
+++ GIS/Animation.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/mrustl/Desktop/WC_Maxflow/trunk/Daten/20161207_DatenausReviermodell_Stand_P014_WR")
+setwd("C:/Users/mrustl/Desktop/WC_Maxflow/trunk/GIS")
 library(shapefiles)
 library(dplyr)
 library(ggplot2)
@@ -11,26 +11,36 @@ library(gganimate)
 stauer <- shapefiles::read.dbf(dbf.name = "Stauer.dbf")$dbf
 entnahme <- shapefiles::read.dbf(dbf.name = "W_Brunnen_bis2015.dbf")$dbf
 
+
 entnahme_pro_jahr_und_brunnen <- entnahme %>% 
- select(Iz, Qbr, X_WERT, Y_WERT) %>% 
+ select(Iz, Qbr, Brgwl, Brkenn, X_WERT, Y_WERT) %>% 
  mutate(Year = Iz + 1970, 
         daysPerYear = lubridate::yday(as.Date(sprintf("%s-12-31", Year, format="%Y-%m-%d"))),
-         Q_perYear = Qbr*60*24*daysPerYear) %>%  
- group_by(Year, X_WERT, Y_WERT) %>%  
- summarise(Q_perYear=-sum(Q_perYear))
+         Q_perYear = Qbr*60*24*daysPerYear, 
+        Randbrunnen = stringr::str_detect(string = entnahme$Brkenn,pattern = "T  WR|T  WS")) %>%  
+ filter(Brgwl == "xx0987xxxxxx", 
+        Randbrunnen == FALSE,
+        X_WERT < 2532000,
+        X_WERT >= 2528500,
+        Y_WERT < 5662600,
+        Y_WERT >= 5656900, 
+        Year >= 2007) %>% 
+ group_by(Brkenn, X_WERT, Y_WERT,Year) %>%  
+ summarise(Q_perYear=-sum(Q_perYear)) 
 
 
 entnahme_pro_jahr <- entnahme_pro_jahr_und_brunnen %>% 
   ungroup() %>% 
   group_by(Year) %>% 
   summarise(Gesamtfoerderung = sum(Q_perYear)) %>% 
-  mutate(label = sprintf("Jahr: %d (Gesamtförderung: %3.1f Millionen m3)", 
+  mutate(label = sprintf("Jahr: %d (Gesamtförderung 6B & 6D: %3.1f Millionen m3)", 
                          Year, 
                          round(Gesamtfoerderung/1000000,1)))
 
 
 entnahme_pro_jahr_und_brunnen <- entnahme_pro_jahr_und_brunnen %>% 
                                   left_join(entnahme_pro_jahr)
+
 
 
 p <- ggplot(entnahme_pro_jahr_und_brunnen, aes(x=X_WERT, 
@@ -55,11 +65,6 @@ plot(Q_perYear ~ Year,
      col="blue", 
      las = 1)
 
-p <- ggplot(gapminder, aes(gdpPercap, lifeExp, size = pop, frame = continent)) +
-  geom_point() +
-  scale_x_log10()
-
-gganimate(p,"entnahme.html")
 # 
 #   
 
