099e7c4aa257a3dd7d1b43bb2c3c2d86099e7424
Michael Rustler <michael.rustler@kompetenz-wasser.de>
2017-04-21 11:44:39 +0200

    add support for increasing the well raster

--- create_mnw2_csvs.R
+++ create_mnw2_csvs.R
@@ -69,6 +69,54 @@ add_last_q <- function(df_old_times, # altbrunnen_times
   return(res)
 }
 
+## Hilfsfunktion zur Ermittlung des Brunnenabstandes ##
+get_well_distances <- function(df_nodes,
+                               col_x = "X_WERT",
+                               col_y = "Y_WERT",
+                               col_labels = "Brkenn") {
+  
+  distanceMatrix <- round(dist(df_nodes[,c(col_x,col_y)],
+                               method = "euclidian", 
+                               upper = TRUE,
+                               diag = FALSE),
+                          1)
+  attr(distanceMatrix, "Labels") <- df_nodes[,col_labels] %>% unlist(use.names = FALSE)
+  
+  
+  dist <- melt(as.matrix(distanceMatrix), varnames = c("row", "col"))
+  dist$row <- as.character(dist$row)
+  dist$col <- as.character(dist$col)
+  
+  dist <- dist %>% 
+    dplyr::filter(value > 0) %>% 
+    dplyr::arrange(row, value) 
+  
+  return(dist)
+}
+
+## Vergrößern des Brunnenrasters (nur Brunnen deren Abstand > krit. Distanz ist!)
+get_remaining_wells <- function(df_nodes,
+                                critDist = 300) {
+  
+  well_distances <- get_well_distances(df_nodes = df_nodes)
+  
+  dist_df <- well_distances %>% 
+    dplyr::filter(value < critDist) %>% 
+    dplyr::arrange(row, value) 
+  
+  wells_to_remain <- unique(dist_df$row)
+  
+  if (length(wells_to_remain) == 0) {
+    msg <- sprintf("Fehler: keine Neubrunnen erfüllen das Kriterium: Entfernung <= %4.0f m.\n
+Bitte einen größeren Wert für Parameter 'critDist' in Funktion get_remaining_wells() wählen!", 
+                   critDist)
+    stop(msg)
+    }
+  return(list(names = wells_to_remain,
+              distanceMatrix = dist_df))
+}
+
+
 ## Einlesen der Brunnen für Kalibrierungszeitraum ##
 
 altbrunnen <- read.csv("GIS/alleBrunnen_BIOS_2007-2015.csv",
@@ -120,6 +168,7 @@ neubrunnen_nodes <- read.csv("GIS/alleBrunnen_2016-2018.csv",
   dplyr::summarise(X_WERT = min(X_WERT),
                    Y_WERT = min(Y_WERT)) 
 
+
 ## Einlesen der Förderraten für Prognoseszeitraum ##
 
 neubrunnen_times <- read.csv("GIS/alleBrunnen_Wabis_bis 2020.csv") %>%  
@@ -130,7 +179,58 @@ neubrunnen_times <- read.csv("GIS/alleBrunnen_Wabis_bis 2020.csv") %>%
 
 
 neubrunnen_nodes <- neubrunnen_nodes %>% 
-  dplyr::filter(Brkenn %in% unique(neubrunnen_times$Brkenn)) %>% 
+  dplyr::filter(Brkenn %in% unique(neubrunnen_times$Brkenn))
+
+################################################################################
+## Vergrößern des Brunnenrasters##
+################################################################################
+
+#### Hilfs-Abbildung zur Bestimmung eines optimalen Wertes für 'critDist'  
+if (FALSE) {
+  
+  critDist <- seq(50,300,5)
+  
+  
+  wells <- vector()
+  i <- 0
+  for (dist in critDist) {
+    i <- i + 1 
+    wells[i] <- length(get_remaining_wells(df_nodes = neubrunnen_nodes %>% filter(k == 0),
+                                           critDist = dist)$names)
+  }
+  
+  plot(x = critDist,y = wells,
+       ylim = rev(range(wells)), 
+       xlab = "Brunnenabstand [m]", 
+       ylab = "Anzahl Brunnen",
+       las = 1)
+}
+
+# Berücksichtigt nur Brunnen deren Abstände > krit. Entfernung ist 
+# (falls Parameter 'critDist' zu klein gewählt -> FEHLER, daher Abschätzung
+# zu sinnvollen Wertebereich mittels obigem Plot durchführen!!!!)
+
+critDist <- 150 ### hier ändern um Brunnenraster anzupassen!!!!!!!!!!
+
+neubrunnen_remaining <- get_remaining_wells(df_nodes = neubrunnen_nodes %>% 
+                                            filter(k == 0),
+                                            critDist = critDist)
+
+## Nur zum Testen -> Entfernungsmatrix
+#neubrunnen_remaining$distanceMatrix
+
+
+### Eliminieren von Neubrunnen die nicht 'critDist' Kriterium erfüllen
+neubrunnen_nodes <- neubrunnen_nodes %>%  
+                    filter(Brkenn %in% neubrunnen_remaining$names)
+
+neubrunnen_times <- neubrunnen_times %>%  
+                    filter(Brkenn %in% neubrunnen_remaining$names)
+################################################################################
+################################################################################
+################################################################################
+
+neubrunnen_nodes <- neubrunnen_nodes %>% 
   dplyr::ungroup() %>% 
   dplyr::mutate(wellid = sprintf("welll%d",
                                  (nrow(altbrunnen_nodes) + 1):(nrow(altbrunnen_nodes) + n())))
@@ -175,51 +275,7 @@ wells_times <- wells_time_dummy(wells_nodes,pers = 0:max_per) %>%
 
 #wells_times %>% group_by(wellid,per) %>% summarise(n = n()) %>% filter(n > 1) %>% View()
 
-## Änderung des Brunnenrasters ##
 
-if (FALSE) {
-  
-well_number <- function(critDist = 200,
-                        df = neubrunnen_nodes %>% filter(k == 0)) {
-
-  distanceMatrix <- round(dist(df[,c("X_WERT","Y_WERT")],
-                             method = "euclidian", 
-                             upper = TRUE,
-                             diag = FALSE),
-                        1)
-attr(distanceMatrix, "Labels") <- df$Brkenn
-# cat("Well distance matrix table:")
-# cat("")
-# distanceMatrix
-
-
-
-df <- melt(as.matrix(distanceMatrix), varnames = c("row", "col"))
-df$row <- as.character(df$row)
-df$col <- as.character(df$col)
-
-df <- df %>% dplyr::filter(value > 0) %>% dplyr::arrange(row, value) 
-
-res <- df %>% dplyr::filter(value < critDist) %>% 
-  dplyr::arrange(row, value) 
-
-length(unique(unique(res$row), unique(res$col)))
-
-}
-
-critDist <- seq(50,300,5)
-
-wells <- vector()
-i <- 0
-for(dist in critDist) {
-i <- i + 1 
-wells[i] <- well_number(dist)
-}
-
-plot(x = critDist,y = wells,
-     ylim = rev(range(wells)), 
-     xlab="Brunnenabstand [m]", ylab="Anzahl Brunnen",)
-}
 
 
 ###############################
